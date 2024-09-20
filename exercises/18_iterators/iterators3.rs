2b56130547e60dc29cf3377ea614ddb5ffb5d5fb
Harry Chen <baffen227@gmail.com>
2024-09-20 17:13:35 +0800

    Finish iterators3

--- exercises/18_iterators/iterators3.rs
+++ exercises/18_iterators/iterators3.rs
@@ -13,78 +13,103 @@
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum DivisionError {
-    NotDivisible(NotDivisibleError),
-    DivideByZero,
+	NotDivisible(NotDivisibleError),
+	DivideByZero,
 }
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct NotDivisibleError {
-    dividend: i32,
-    divisor: i32,
+	dividend: i32,
+	divisor: i32,
 }
 
 // Calculate `a` divided by `b` if `a` is evenly divisible by `b`.
 // Otherwise, return a suitable error.
 pub fn divide(a: i32, b: i32) -> Result<i32, DivisionError> {
-    todo!();
+	match a.checked_rem_euclid(b) {
+		None => Err(DivisionError::DivideByZero),
+		Some(r) => {
+			if r != 0 {
+				Err(DivisionError::NotDivisible(NotDivisibleError {
+					dividend: a,
+					divisor: b,
+				}))
+			} else {
+				Ok(a / b)
+			}
+		}
+	}
 }
 
 // Complete the function and return a value of the correct type so the test
 // passes.
 // Desired output: Ok([1, 11, 1426, 3])
-fn result_with_list() -> () {
-    let numbers = vec![27, 297, 38502, 81];
-    let division_results = numbers.into_iter().map(|n| divide(n, 27));
+fn result_with_list() -> Result<[i32; 4], DivisionError> {
+	let numbers = vec![27, 297, 38502, 81];
+	let mut answers = [0_i32; 4];
+	numbers
+		.into_iter()
+		.map(|n| divide(n, 27).unwrap())
+		.enumerate()
+		.for_each(|(i, n)| answers[i] = n);
+	Ok(answers)
 }
 
 // Complete the function and return a value of the correct type so the test
 // passes.
 // Desired output: [Ok(1), Ok(11), Ok(1426), Ok(3)]
-fn list_of_results() -> () {
-    let numbers = vec![27, 297, 38502, 81];
-    let division_results = numbers.into_iter().map(|n| divide(n, 27));
+fn list_of_results() -> [Result<i32, DivisionError>; 4] {
+	let numbers = vec![27, 297, 38502, 81];
+	const ARRAY_REPEAT_VALUE: Result<i32, DivisionError> = Ok(0);
+	let mut answers: [Result<i32, DivisionError>; 4] = [ARRAY_REPEAT_VALUE; 4];
+	numbers
+		.into_iter()
+		.map(|n| divide(n, 27))
+		.enumerate()
+		.for_each(|(i, n)| answers[i] = n);
+	answers
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+	use super::*;
 
-    #[test]
-    fn test_success() {
-        assert_eq!(divide(81, 9), Ok(9));
-    }
+	#[test]
+	fn test_success() {
+		assert_eq!(divide(81, 9), Ok(9));
+	}
 
-    #[test]
-    fn test_not_divisible() {
-        assert_eq!(
-            divide(81, 6),
-            Err(DivisionError::NotDivisible(NotDivisibleError {
-                dividend: 81,
-                divisor: 6
-            }))
-        );
-    }
+	#[test]
+	fn test_not_divisible() {
+		assert_eq!(
+			divide(81, 6),
+			Err(DivisionError::NotDivisible(NotDivisibleError {
+				dividend: 81,
+				divisor: 6
+			}))
+		);
+	}
 
-    #[test]
-    fn test_divide_by_0() {
-        assert_eq!(divide(81, 0), Err(DivisionError::DivideByZero));
-    }
+	#[test]
+	fn test_divide_by_0() {
+		assert_eq!(divide(81, 0), Err(DivisionError::DivideByZero));
+	}
 
-    #[test]
-    fn test_divide_0_by_something() {
-        assert_eq!(divide(0, 81), Ok(0));
-    }
+	#[test]
+	fn test_divide_0_by_something() {
+		assert_eq!(divide(0, 81), Ok(0));
+	}
 
-    #[test]
-    fn test_result_with_list() {
-        assert_eq!(format!("{:?}", result_with_list()), "Ok([1, 11, 1426, 3])");
-    }
+	#[test]
+	fn test_result_with_list() {
+		assert_eq!(format!("{:?}", result_with_list()), "Ok([1, 11, 1426, 3])");
+	}
 
-    #[test]
-    fn test_list_of_results() {
-        assert_eq!(
-            format!("{:?}", list_of_results()),
-            "[Ok(1), Ok(11), Ok(1426), Ok(3)]"
-        );
-    }
+	#[test]
+	fn test_list_of_results() {
+		assert_eq!(
+			format!("{:?}", list_of_results()),
+			"[Ok(1), Ok(11), Ok(1426), Ok(3)]"
+		);
+	}
 }
